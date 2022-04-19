c4827db69f0a04e4071d8e9935fb215528bb548d
abelAsres <abelasres35@gmail.com>
2022-04-19 15:23:54 -0400

    set Order and Payment models

--- src/main/java/com/revature/spaceecobackend/model/Order.java
+++ src/main/java/com/revature/spaceecobackend/model/Order.java
@@ -1,4 +1,51 @@
 package com.revature.spaceecobackend.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+/*
+    // seller id
+    @JoinColumn(name="seller_id",nullable = false)
+    int sellerId;
+
+    // buyer id
+    @JoinColumn(name="buyer_id", nullable = false)
+    @OneToOne
+    private int buyerId;
+
+    list of products
+    @ManyToMany
+    List<Product>items
+*/
+    // order date
+    @Column(name="order_date",nullable = false)
+    private Timestamp orderDate;
+
+    // order status
+    @Column(name="order_status", nullable = false)
+    private String orderStatus;
+
+    // shipping address
+    @ManyToOne
+    @JoinColumn(name ="shipping_address_id", nullable = false)
+    private int shippingAddressId;
+
+    // payment id
+    @JoinColumn(name="payment_id")
+    @OneToOne
+    private Payment payment;
 }
--- src/main/java/com/revature/spaceecobackend/model/Payment.java
+++ src/main/java/com/revature/spaceecobackend/model/Payment.java
@@ -1,4 +1,28 @@
 package com.revature.spaceecobackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="payments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "billing_address_id")
+    @OneToOne
+    private int billingId;
+
+    @Column(name="payment_type", nullable=false)
+    private String type;
+
+    @Column(name="payment_status", nullable=false)
+    private String status;
 }
