539fb8b70d6c8843bca4c0bdf20fdeafccf80d1f
tientle84 <alctdl00@gmail.com>
2022-04-19 15:14:00 -0400

    Add UserRole model

---
+++ src/main/java/com/revature/spaceecobackend/model/UserRole.java
@@ -0,0 +1,36 @@
+package com.revature.spaceecobackend.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_role_id")
+    private int id;
+
+    @Column(name="user_role")
+    private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return id == userRole.id && Objects.equals(role, userRole.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, role);
+    }
+}
