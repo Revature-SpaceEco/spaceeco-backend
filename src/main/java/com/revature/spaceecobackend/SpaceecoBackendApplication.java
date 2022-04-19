97c8d065aaf4b01580385953297563d91bbad7c9
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-04-19 11:56:22 -0400

    created backend file structure

---
+++ src/main/java/com/revature/spaceecobackend/SpaceecoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.spaceecobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceecoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceecoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/spaceecobackend/SpaceecoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.spaceecobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceecoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
