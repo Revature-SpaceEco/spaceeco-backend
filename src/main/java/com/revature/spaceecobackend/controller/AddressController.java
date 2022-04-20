49f301ffb29002f9ed5aa075d4dec4157e6f6009
Gizu007 <giancarlossucre98@hotmail.com>
2022-04-20 12:58:17 -0400

    Added controller layer and service layer

--- src/main/java/com/revature/spaceecobackend/controller/AddressController.java
+++ src/main/java/com/revature/spaceecobackend/controller/AddressController.java
@@ -3,6 +3,9 @@ package com.revature.spaceecobackend.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import com.revature.spaceecobackend.model.Address;
+import com.revature.spaceecobackend.service.AddressService;
+import com.revature.spaceecobackend.service.AddressServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,37 +13,44 @@ import java.util.Optional;
 @RestController("/address")
 public class AddressController {
 
-    //add address service as private instance variable @Autowired
+  /*  //add address service as private instance variable @Autowired
 
     //add users as private instance variable @Autowired
-    /*
-    @PostMapping
-    public ResponseEntity<?> createAddress(@RequestBody Address address) throws JsonProcessingException{ Create
-        return addressService.createAddress;
 
-    }*/
-    /*
+    @Autowired
+    AddressServiceInterface addressService;
+
+
+
+    @PostMapping
+    public ResponseEntity<?> createAddress(@RequestBody Address address) throws JsonProcessingException{ //Create
+        Address address1 = addressService.createAddress(address);
+        return ResponseEntity.status(200).body(address1);
+
+    }
+
     @GetMapping("/{id}")
     public Optional<Address> getAddressByUserId(@PathVariable String id) { //change name if naming convention changes //Response
-        return addressService.getAddressByUserId(Long.parseLong(id));
-    }*/
-    /*
+        Address address1 = addressService.getAddressByUserId(Long.parseLong(id));
+        return Optional.status(200).body(address1);;
+    }
+
     @DeleteMapping("/{id}")
-    public Boolean deleteAddressById(@PathVariable String id){ Delete
+    public Boolean deleteAddressById(@PathVariable String id){ //Delete
         return addressService.deleteAddress(Long.parseLong(id));
-    }*/
-    /*
+    }
+
     @PutMapping
     public Address updateAddress(@PathVariable String id,
-                                 @RequestBody Address address){ Update
+                                 @RequestBody Address address){ //Update
         return addressService.updateAddressByUserId(Long.parseLong(id), address);
-    } */
+    }
 
-        /*
+
 
     @PatchMapping
     public Address updateAddress2(@PathVariable String id,
-                                 @RequestBody Address address){ Update
+                                 @RequestBody Address address){ //
         return addressService.updateAddressByUserId2(Long.parseLong(id));
-    } */
-}
+    }
+}*/
--- src/main/java/com/revature/spaceecobackend/service/AddressService.java
+++ src/main/java/com/revature/spaceecobackend/service/AddressService.java
@@ -3,6 +3,7 @@ package com.revature.spaceecobackend.service;
 import com.revature.spaceecobackend.dao.AddressRepository;
 import com.revature.spaceecobackend.model.Address;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,29 +12,24 @@ import java.util.Optional;
 public class AddressService implements AddressServiceInterface{
 
     @Autowired
-    private AddressRepository addressRepository;
+    AddressRepository addressRepository;
 
-    @Override
     public Address createAddress(Address address) {
         return addressRepository.save(address);
     }
 
-    @Override
     public Optional<?> getAddress(int id) {
         return Optional.empty();
     }
 
-    @Override
     public Boolean deleteAddress(int id) {
         return null;
     }
 
-    @Override
     public AddressService updateAddressByUserId(int id, Address address) {
         return null;
     }
 
-    @Override
     public AddressService updateAddressByUserId2(int id, Address address) {
         return null;
     }
