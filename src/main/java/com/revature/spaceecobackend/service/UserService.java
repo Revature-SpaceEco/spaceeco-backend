1b18061b98a57da8184af0147ce34ca928139a1f
Nikoloi Ellis <ellisnikoloi@yahoo.com>
2022-04-20 11:39:22 -0400

    finished up the User Model

--- src/main/java/com/revature/spaceecobackend/service/UserService.java
+++ src/main/java/com/revature/spaceecobackend/service/UserService.java
@@ -5,12 +5,17 @@ import com.revature.spaceecobackend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Transactional
     public User createUser(User user){
+
         return userRepository.save(user);
     }
 
