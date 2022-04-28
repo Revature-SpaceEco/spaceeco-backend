7e0ce8fc6973bfa771e5a8f3bef5e29552d7f91d
George Balch <george089@revature.net>
2022-04-27 19:28:35 -0700

    Disable CSRF until it can be properly set up

--- src/main/java/com/revature/spaceecobackend/SecurityConfiguration.java
+++ src/main/java/com/revature/spaceecobackend/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // TODO change
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/users/{userId}/**")
                 .access("@userSecurity.hasUserId(authentication, #userId)")
