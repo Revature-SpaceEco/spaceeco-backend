1832efdc50f23ae70ef8d7378de6337996164e1f
George Balch <george089@revature.net>
2022-04-29 09:21:05 -0700

    Add user id to authenticate response

--- src/main/java/com/revature/spaceecobackend/model/AuthenticationResponse.java
+++ src/main/java/com/revature/spaceecobackend/model/AuthenticationResponse.java
@@ -3,14 +3,21 @@ package com.revature.spaceecobackend.model;
 public class AuthenticationResponse {
 
     private String jwt;
+    private Integer userId;
 
     public AuthenticationResponse() {}
 
-    public AuthenticationResponse(String jwt) {
+    public AuthenticationResponse(String jwt, Integer userId) {
         this.jwt = jwt;
+        this.userId = userId;
     }
 
     public String getJwt() {
         return jwt;
     }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
 }
