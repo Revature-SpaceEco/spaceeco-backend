cb13258b2c80fb3c70ca974209571bcf3b42da90
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-05-03 09:52:04 -0700

    code smell removed commented code

--- src/main/java/com/revature/spaceecobackend/controller/UserController.java
+++ src/main/java/com/revature/spaceecobackend/controller/UserController.java
@@ -2,7 +2,6 @@ package com.revature.spaceecobackend.controller;
 
 import com.revature.spaceecobackend.dto.RegisterUserDTO;
 import com.revature.spaceecobackend.dto.UserDTO;
-import com.revature.spaceecobackend.exception.NotFound;
 import com.revature.spaceecobackend.model.User;
 import com.revature.spaceecobackend.service.MfaService;
 import com.revature.spaceecobackend.service.UserService;
@@ -30,44 +29,25 @@ public class UserController {
   private MfaService mfaService;
 
   @PostMapping()
-  public ResponseEntity<?> addUser(@RequestBody RegisterUserDTO registerUserDTO) throws QrGenerationException, NotFound {
+  public ResponseEntity<?> addUser(@RequestBody RegisterUserDTO registerUserDTO) throws QrGenerationException {
 
-    try{
-      ModelMapper modelMapper = new ModelMapper();
-      User user = modelMapper.map(registerUserDTO, User.class);
+    ModelMapper modelMapper = new ModelMapper();
+    User user = modelMapper.map(registerUserDTO, User.class);
 
-      user.setActive(true);
-      user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
-      user.setSecret(mfaService.getSecret());
-      String qrCode = mfaService.getQrCode(user.getSecret(), user.getEmail());
+    user.setActive(true);
+    user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
+    user.setSecret(mfaService.getSecret());
+    String qrCode = mfaService.getQrCode(user.getSecret(), user.getEmail());
 
-      UserDTO returnUser = userService.createUser(user);
+    UserDTO returnUser = userService.createUser(user);
 
-      returnUser.setQrCode(qrCode);
+    if (returnUser != null) {
+        returnUser.setQrCode(qrCode);
 
-      return ResponseEntity.status(200).body(returnUser);
-
-
-    }catch (NotFound e){
-      return ResponseEntity.status(400).body(e.getMessage());
+        return ResponseEntity.status(200).body(returnUser);
+    } else {
+        return ResponseEntity.status(400).body("Registration Failed");
     }
-//    ModelMapper modelMapper = new ModelMapper();
-//    User user = modelMapper.map(registerUserDTO, User.class);
-//
-//    user.setActive(true);
-//    user.setPassword(passwordEncoder.encode(registerUserDTO.getPassword()));
-//    user.setSecret(mfaService.getSecret());
-//    String qrCode = mfaService.getQrCode(user.getSecret(), user.getEmail());
-//
-//    UserDTO returnUser = userService.createUser(user);
-//
-//    if (returnUser != null) {
-//        returnUser.setQrCode(qrCode);
-//
-//        return ResponseEntity.status(200).body(returnUser);
-//    } else {
-//        return ResponseEntity.status(400).body("Registration Failed");
-//    }
   }
 
   @GetMapping()
